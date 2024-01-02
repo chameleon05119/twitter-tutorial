315c1a2dfa737b2e35bc0c75579acec92971bd6d
chameleon05119 <chameleon05119@gmail.com>
2024-01-02 22:19:51 +0900

    サイドバーロゴを追加

--- components/layout/SideBar.tsx
+++ components/layout/SideBar.tsx
@@ -1,5 +1,36 @@
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+
+import SidebarLodo from "@/components/layout/SidebarLodo";
+
 const SideBar = () => {
-  return <div></div>;
+  const items = [
+    {
+      label: "Home",
+      href: "/",
+      icon: BsHouseFill,
+    },
+    {
+      label: "Notification",
+      href: "/notification",
+      icon: BsBellFill,
+    },
+    {
+      label: "Profile",
+      href: "/users/123",
+      icon: FaUser,
+    },
+  ];
+
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLodo />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
