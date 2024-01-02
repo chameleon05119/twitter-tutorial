215cf2afddeb902c364adbffd63687549d5ac2cd
chameleon05119 <chameleon05119@gmail.com>
2024-01-02 22:54:38 +0900

    サイドバーアイテムコンポーネントの編集

--- components/layout/SideBarItem.tsx
+++ components/layout/SideBarItem.tsx
@@ -1,5 +1,29 @@
-const SideBarItem = () => {
-  return <div></div>;
+import { IconType } from "react-icons";
+
+interface SideBarItemProrps {
+  label: string;
+  href?: string;
+  icon: IconType;
+  onClick?: () => void;
+}
+
+const SideBarItem: React.FC<SideBarItemProrps> = ({
+  label,
+  href,
+  icon: Icon,
+  onClick,
+}) => {
+  return (
+    <div className="flex flex-row items-center">
+      <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
+        <Icon size={28} color="white" />
+      </div>
+      <div className="relative hidden lg:flex items-center gap-4 p-4 rounded-full hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer">
+        <Icon size={24} color="white" />
+        <p className="hidden lg:black text-white text-xl">{label}</p>
+      </div>
+    </div>
+  );
 };
 
 export default SideBarItem;
