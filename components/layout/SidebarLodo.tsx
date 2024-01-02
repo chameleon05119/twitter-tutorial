5a92cd650ec5db9cba0776adf46d133654b72f08
chameleon05119 <chameleon05119@gmail.com>
2024-01-02 22:21:12 +0900

    アイコンクリック時にHOMEに遷移する

--- components/layout/SidebarLodo.tsx
+++ components/layout/SidebarLodo.tsx
@@ -5,7 +5,10 @@ const SidebarLodo = () => {
   const router = useRouter();
 
   return (
-    <div className="rounded-full h-14 w-14 p-4 flex items-center justify-center hover:bg-blue-30 hover:bg-opacity-10 cursor-pointer transition">
+    <div
+      onClick={() => router.push("/")}
+      className="rounded-full h-14 w-14 p-4 flex items-center justify-center hover:bg-blue-30 hover:bg-opacity-10 cursor-pointer transition"
+    >
       <BsTwitter size={28} color="white" />
     </div>
   );
